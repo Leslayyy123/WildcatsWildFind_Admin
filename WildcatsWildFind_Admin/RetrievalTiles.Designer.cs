89d89d350d43415c94311c1722ff33b7f2c1c1e4
Amiel Grant Cogtas <amielgrant.cogtas@cit.edu>
2024-12-16 07:46:34 +0800

    .

--- WildcatsWildFind_Admin/RetrievalTiles.Designer.cs
+++ WildcatsWildFind_Admin/RetrievalTiles.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             pictureBox1 = new PictureBox();
             label1 = new Label();
             label2 = new Label();
@@ -52,8 +52,9 @@
             // pictureBox1
             // 
             pictureBox1.Location = new Point(-1, -1);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(289, 155);
+            pictureBox1.Size = new Size(253, 116);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             // 
@@ -61,9 +62,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 168);
+            label1.Location = new Point(10, 126);
             label1.Name = "label1";
-            label1.Size = new Size(55, 25);
+            label1.Size = new Size(45, 20);
             label1.TabIndex = 1;
             label1.Text = "INFO";
             // 
@@ -71,9 +72,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(13, 211);
+            label2.Location = new Point(11, 158);
             label2.Name = "label2";
-            label2.Size = new Size(54, 20);
+            label2.Size = new Size(42, 15);
             label2.TabIndex = 2;
             label2.Text = "Name:";
             // 
@@ -82,9 +83,9 @@
             lblName.AutoSize = true;
             lblName.Font = new Font("Bahnschrift Condensed", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblName.ForeColor = SystemColors.ControlDark;
-            lblName.Location = new Point(72, 213);
+            lblName.Location = new Point(63, 160);
             lblName.Name = "lblName";
-            lblName.Size = new Size(57, 18);
+            lblName.Size = new Size(47, 14);
             lblName.TabIndex = 3;
             lblName.Text = "Full Name";
             // 
@@ -92,9 +93,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label4.Location = new Point(13, 233);
+            label4.Location = new Point(11, 175);
             label4.Name = "label4";
-            label4.Size = new Size(119, 20);
+            label4.Size = new Size(91, 15);
             label4.TabIndex = 4;
             label4.Text = "Phone Number:";
             // 
@@ -103,9 +104,9 @@
             lblNumber.AutoSize = true;
             lblNumber.Font = new Font("Bahnschrift Condensed", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblNumber.ForeColor = SystemColors.ControlDark;
-            lblNumber.Location = new Point(138, 235);
+            lblNumber.Location = new Point(121, 176);
             lblNumber.Name = "lblNumber";
-            lblNumber.Size = new Size(71, 18);
+            lblNumber.Size = new Size(58, 14);
             lblNumber.TabIndex = 5;
             lblNumber.Text = "Your Number";
             // 
@@ -113,9 +114,9 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label6.Location = new Point(13, 253);
+            label6.Location = new Point(11, 190);
             label6.Name = "label6";
-            label6.Size = new Size(76, 20);
+            label6.Size = new Size(60, 15);
             label6.TabIndex = 6;
             label6.Text = "Date Lost:";
             // 
@@ -124,9 +125,9 @@
             lblDateLost.AutoSize = true;
             lblDateLost.Font = new Font("Bahnschrift Condensed", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblDateLost.ForeColor = SystemColors.ControlDark;
-            lblDateLost.Location = new Point(94, 256);
+            lblDateLost.Location = new Point(82, 192);
             lblDateLost.Name = "lblDateLost";
-            lblDateLost.Size = new Size(54, 18);
+            lblDateLost.Size = new Size(43, 14);
             lblDateLost.TabIndex = 7;
             lblDateLost.Text = "Date Lost";
             // 
@@ -134,9 +135,9 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label8.Location = new Point(13, 292);
+            label8.Location = new Point(11, 219);
             label8.Name = "label8";
-            label8.Size = new Size(91, 20);
+            label8.Size = new Size(71, 15);
             label8.TabIndex = 8;
             label8.Text = "Description:";
             // 
@@ -145,9 +146,9 @@
             lblDesc.AutoSize = true;
             lblDesc.Font = new Font("Bahnschrift Condensed", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
             lblDesc.ForeColor = SystemColors.ControlDark;
-            lblDesc.Location = new Point(72, 312);
+            lblDesc.Location = new Point(63, 234);
             lblDesc.Name = "lblDesc";
-            lblDesc.Size = new Size(202, 80);
+            lblDesc.Size = new Size(169, 65);
             lblDesc.TabIndex = 9;
             lblDesc.Text = "and completing requests efficiently, ensuring \r\ntimely communication andupdates. With cus\r\ntomizable featuresand robust support, You\r\nrRequest y.\r\n\r\n";
             // 
@@ -155,7 +156,7 @@
             // 
             btnRelease.BackColor = Color.Transparent;
             btnRelease.BorderRadius = 15;
-            btnRelease.CustomizableEdges = customizableEdges5;
+            btnRelease.CustomizableEdges = customizableEdges1;
             btnRelease.DisabledState.BorderColor = Color.DarkGray;
             btnRelease.DisabledState.CustomBorderColor = Color.DarkGray;
             btnRelease.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
@@ -163,47 +164,49 @@
             btnRelease.FillColor = Color.Maroon;
             btnRelease.Font = new Font("Segoe UI", 9F);
             btnRelease.ForeColor = Color.White;
-            btnRelease.Location = new Point(138, 392);
+            btnRelease.Location = new Point(121, 294);
+            btnRelease.Margin = new Padding(3, 2, 3, 2);
             btnRelease.Name = "btnRelease";
             btnRelease.ShadowDecoration.BorderRadius = 15;
-            btnRelease.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            btnRelease.ShadowDecoration.CustomizableEdges = customizableEdges2;
             btnRelease.ShadowDecoration.Depth = 20;
             btnRelease.ShadowDecoration.Enabled = true;
             btnRelease.ShadowDecoration.Shadow = new Padding(0, 0, 8, 8);
-            btnRelease.Size = new Size(120, 31);
+            btnRelease.Size = new Size(105, 23);
             btnRelease.TabIndex = 10;
-            btnRelease.Text = "Realease";
+            btnRelease.Text = "Release";
             // 
             // guna2Panel1
             // 
             guna2Panel1.BackColor = Color.Transparent;
             guna2Panel1.BorderRadius = 10;
             guna2Panel1.Controls.Add(label10);
-            customizableEdges7.BottomRight = false;
-            customizableEdges7.TopLeft = false;
-            guna2Panel1.CustomizableEdges = customizableEdges7;
+            customizableEdges3.BottomRight = false;
+            customizableEdges3.TopLeft = false;
+            guna2Panel1.CustomizableEdges = customizableEdges3;
             guna2Panel1.FillColor = Color.FromArgb(240, 181, 4);
-            guna2Panel1.Location = new Point(2, 3);
+            guna2Panel1.Location = new Point(2, 2);
+            guna2Panel1.Margin = new Padding(3, 2, 3, 2);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            guna2Panel1.Size = new Size(113, 25);
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Panel1.Size = new Size(99, 19);
             guna2Panel1.TabIndex = 11;
             // 
             // label10
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label10.Location = new Point(22, 1);
+            label10.Location = new Point(19, 1);
             label10.Name = "label10";
-            label10.Size = new Size(56, 20);
+            label10.Size = new Size(44, 15);
             label10.TabIndex = 12;
             label10.Text = "Laptop";
             // 
             // RetrievalTiles
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(288, 435);
+            ClientSize = new Size(252, 326);
             Controls.Add(guna2Panel1);
             Controls.Add(btnRelease);
             Controls.Add(lblDesc);
@@ -217,6 +220,7 @@
             Controls.Add(label1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 2, 3, 2);
             Name = "RetrievalTiles";
             Text = "RetrievalTiles";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
