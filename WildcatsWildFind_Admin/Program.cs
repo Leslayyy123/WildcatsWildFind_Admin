611579d2fc68dc459c741f38760c47c2aece9cb7
jaspher siloy <jaspher siloy@LAPTOP-GMR53LPQ>
2024-12-17 15:00:07 +0800

    update

--- WildcatsWildFind_Admin/Program.cs
+++ WildcatsWildFind_Admin/Program.cs
@@ -11,7 +11,8 @@ namespace WildcatsWildFind_Admin
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AdminLogin());
+            //Application.Run(new AdminLogin());
+            Application.Run(new Dashboard());
         }
     }
 }
